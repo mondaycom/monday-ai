e75de91df076a53f91b51019791b443c3814c39a
Ori Lael <orila@monday.com>
2025-04-14 16:16:37 +0300

    Change index.js error

--- packages/agent-toolkit/src/core/tools/index.ts
+++ packages/agent-toolkit/src/core/tools/index.ts
@@ -1,7 +1,7 @@
 import { allGraphqlApiTools, filterApiTools } from './platform-api-tools';
-import { allMondayAppsTools, mondayAppsTools } from './monday-apps-tools';
+import { allMondayAppsTools } from './monday-apps-tools';
 
 // Export all tools for use in the toolkit
 export const allTools = [...allGraphqlApiTools, ...allMondayAppsTools];
 
-export { allGraphqlApiTools, filterApiTools, allMondayAppsTools, mondayAppsTools };
+export { allGraphqlApiTools, filterApiTools, allMondayAppsTools };
